977e3a8fa130bf4ea940ca835a6a2e01fbb7272f
trofimov_d_a <trofimov_d_a@magnit.ru>
2020-01-31 16:59:55 +0300

    initial commit

---
+++ conanfile.py
@@ -0,0 +1,63 @@
+import os
+from conans import ConanFile, CMake, tools
+
+# see https://github.com/conan-io/conan-center-index/tree/master/recipes/c-ares/all
+class CAresConan(ConanFile):
+    name = "c-ares"
+    version = "cares-1_15_0"
+    license = "MIT"
+    url = "https://github.com/conan-io/conan-center-index"
+    description = "A C library for asynchronous DNS requests"
+    topics = ("conan", "c-ares", "dns")
+    homepage = "https://c-ares.haxx.se/"
+    settings = "os_build", "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {'shared': False, 'fPIC': True}
+    exports_sources = "CMakeLists.txt"
+    generators = "cmake", "cmake_paths", "virtualenv"
+    repo_url = 'https://github.com/c-ares/c-ares.git'
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def configure(self):
+        del self.settings.compiler.libcxx
+        del self.settings.compiler.cppstd
+
+    def source(self):
+        #tools.get(**self.conan_data["sources"][self.version])
+        #os.rename("c-ares-cares-{}".format(self.version.replace(".", "_")), "source_folder")
+        self.run('git clone --progress --depth 1 --branch {} --recursive --recurse-submodules {} {}'.format(self.version, self.repo_url, self.source_folder))
+
+    def _cmake_configure(self):
+        cmake = CMake(self)
+        cmake.definitions["CARES_STATIC"] = not self.options.shared
+        cmake.definitions["CARES_SHARED"] = self.options.shared
+        cmake.definitions["CARES_BUILD_TESTS"] = "OFF"
+        cmake.definitions["CARES_MSVC_STATIC_RUNTIME"] = "OFF"
+        cmake.configure(source_folder=self.source_folder, build_folder=self.build_folder)
+        return cmake
+
+    def build(self):
+        cmake = self._cmake_configure()
+        cmake.build()
+        cmake.install()
+
+    def package(self):
+        cmake = self._cmake_configure()
+        cmake.install()
+        self.copy("*LICENSE.md", src=self.source_folder, dst="licenses", keep_path=False)
+
+        tools.rmdir(os.path.join(self.package_folder, 'lib', 'cmake'))
+        tools.rmdir(os.path.join(self.package_folder, 'lib', 'pkgconfig'))
+
+    def package_info(self):
+        self.cpp_info.libs = tools.collect_libs(self)
+        if not self.options.shared:
+            self.cpp_info.defines.append("CARES_STATICLIB")
+        if self.settings.os == "Windows":
+            self.cpp_info.libs.append("ws2_32")
+        self.cpp_info.names['pkg_config'] = 'libcares'
+        self.cpp_info.names["cmake_find_package"] = "c-ares"
+        self.cpp_info.names["cmake_find_package_multi"] = "c-ares"
